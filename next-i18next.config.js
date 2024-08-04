1ba464a4e8f92980240b84a943a483ba8b36737b
markshawn2020 <shawninjuly@gmail.com>
2024-08-04 17:04:05 +0800

    :rocket: before merge into codebase

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
-	debug: false, // process.env.NODE_ENV === 'development',
-	
-	i18n: {
-		defaultLocale: 'zh-CN',
-		locales: ['zh-CN', 'en'],
-		localeDetection: true,
-	},
-	
-	defaultNS: "manufacture",
-	
-	/** To avoid issues when deploying to some paas (vercel...) */
-	localePath:
-		typeof window === 'undefined'
-			? require('path').resolve('./public/locales')
-			: '/locales',
-	
-	reloadOnPrerender: process.env.NODE_ENV === 'development',
+  debug: false, // process.env.NODE_ENV === 'development',
+
+  i18n: {
+    defaultLocale: "zh-CN",
+    locales: ["zh-CN", "en"],
+    // todo: 为什么只能false？ Invalid literal value, expected false at "i18n.localeDetection"
+    localeDetection: false,
+  },
+
+  defaultNS: "manufacture",
+
+  /** To avoid issues when deploying to some paas (vercel...) */
+  localePath:
+    typeof window === "undefined"
+      ? require("path").resolve("./public/locales")
+      : "/locales",
+
+  reloadOnPrerender: process.env.NODE_ENV === "development",
 }
--- src/@types/resources.ts
+++ src/@types/resources.ts
@@ -1,12 +1,9 @@
-import manufacture from '~/locales/zh-CN/manufacture.json'
-import common from '~/locales/zh-CN/common.json'
-
-export type ICommonLocale = typeof common
-export type IManufactureLocale = typeof manufacture
+import common from '../../public/locales/zh-CN/common.json';
+import manufacture from '../../public/locales/zh-CN/manufacture.json';
 
 const resources = {
-	common,
-	manufacture,
-} as const
+  common,
+  manufacture
+} as const;
 
-export default resources
+export default resources;
