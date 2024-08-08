55ce7d58fd7f0a18f31ddde43914decee72dc626
markshawn2020 <shawninjuly@gmail.com>
2024-08-08 16:34:03 +0800

    updated homepage

--- src/config.ts
+++ src/config.ts
@@ -1,36 +1,35 @@
 export const uri = {
-	images: {
-		AiMap: '/images/bg/ai-anatomy-map@0.5x-裁切版.jpg',
-		explore: '/images/mj-xl/home-cover.jpg',
-		carousal: {
-			explore: '/images/mj-xl/carousel-explore.jpg',
-			competition: '/images/mj-xl/carousel-peace.jpg',
-		},
-	},
-	logo: '/images/logo/m/32_white_black.ico',
-	home: '/',
-	auth: {
-		login: '/auth/login',
-		register: '/auth/register',
-	},
-	about: {
-		us: '/about/us',
-		faq: '/about/faq',
-	},
-	product: {
-        sokka: "https://sokka.cs-magic.cn",
-		poketto: 'https://poketto.cs-magic.cn',
-        swot:"https://swot.cs-magic.cn"
-	},
+  images: {
+    AiMap: "/images/bg/ai-anatomy-map@0.5x-裁切版.jpg",
+    explore: "/images/mj-xl/home-cover.jpg",
+    carousal: {
+      explore: "/images/mj-xl/carousel-explore.jpg",
+      competition: "/images/mj-xl/carousel-peace.jpg",
+    },
+  },
+  logo: "/images/logo/m/16_black_content.ico",
+  home: "/",
+  auth: {
+    login: "/auth/login",
+    register: "/auth/register",
+  },
+  about: {
+    us: "/about/us",
+    faq: "/about/faq",
+  },
+  product: {
+    sokka: "https://sokka.cs-magic.cn",
+    poketto: "https://poketto.cs-magic.cn",
+    swot: "https://swot.cs-magic.cn",
+  },
 }
-
 
 export const beian = {
-	domainMatch: /cs-magic.cn/,
-	title: '苏ICP备2023015349号',
+  domainMatch: /cs-magic.cn/,
+  title: "苏ICP备2023015349号",
 }
 
-export const mossSlogan = '-.-. ... -....- -- .- --. .. -.-. .-.-.- .- ..'
+export const mossSlogan = "-.-. ... -....- -- .- --. .. -.-. .-.-.- .- .."
 
 export const LOGO_SIZE_LG = 36
 export const ICON_SIZE_MD = 28
@@ -38,39 +37,24 @@ export const ICON_SIZE_SM = 20
 export const ICON_SIZE_XS = 16
 
 export const groupMembers = {
-	'Founders': [
-		'Mark',
-		'Nicole',
-	],
-	'Core': [
-		'Susan',
-		'Clotho',
-		'Lara',
-		'Cynthia',
-		'Yimi',
-	],
-	'Advisory': [
-		'Nirva',
-		'Joy',
-		'Ella',
-		'Gaosh',
-		'John',
-	],
-	'Community': [
-		'Alone',
-		'Amber',
-		'Ely',
-		'Jaisy',
-		'Jane',
-		'Justine',
-		'LiXi',
-		'LiXiaowen',
-		'Mason',
-		'Ryan',
-		'Precious',
-		'Sarah',
-		'tt',
-		'Yokonsan',
-		'Zeo',
-	],
+  Founders: ["Mark", "Nicole"],
+  Core: ["Susan", "Clotho", "Lara", "Cynthia", "Yimi"],
+  Advisory: ["Nirva", "Joy", "Ella", "Gaosh", "John"],
+  Community: [
+    "Alone",
+    "Amber",
+    "Ely",
+    "Jaisy",
+    "Jane",
+    "Justine",
+    "LiXi",
+    "LiXiaowen",
+    "Mason",
+    "Ryan",
+    "Precious",
+    "Sarah",
+    "tt",
+    "Yokonsan",
+    "Zeo",
+  ],
 }
