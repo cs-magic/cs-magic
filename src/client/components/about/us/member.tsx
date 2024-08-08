e45d69c0e17360a1fca7da0bf0393f0798bacf3c
markshawn2020 <shawninjuly@gmail.com>
2024-08-08 21:44:48 +0800

    fixed member page

--- src/client/components/about/us/member.tsx
+++ src/client/components/about/us/member.tsx
@@ -1,74 +1,103 @@
-import { clsx } from 'clsx'
-import Image from 'next/image'
-import ImgMoZhen from '~/images/members/mozhen.png'
-import { useTranslation } from 'next-i18next'
-import { LocaleNameSpace } from '@/ds/locales'
-import { ICommonLocale } from '@/@types/resources'
-import { Separator } from '@/client/components/ui/separator'
-import Link from 'next/link'
-import { ReactNode } from 'react'
+import { clsx } from "clsx"
+import Image from "next/image"
+import ImgMoZhen from "~/images/members/mozhen.png"
+import { useTranslation } from "next-i18next"
+import { LocaleNameSpace } from "@/ds/locales"
+import resources from "@/@types/resources"
+import { Separator } from "@/client/components/ui/separator"
+import Link from "next/link"
+import { ReactNode } from "react"
 
 const MemberProperty = (props: {
-	name: keyof ICommonLocale['Team']['Member']
-	value?: ReactNode
+  name: keyof typeof resources.common.Team.Member
+  value?: ReactNode
 }) => {
-	const { t } = useTranslation()
-	
-	if (!props.value) return null
-	
-	return (
-		<tr className={'text-sm flex space-x-2'}>
-			<td className={'w-[2.5rem] shrink-0'}>{t(`common:Team.Member.${props.name}`)}</td>
-			<td><Separator orientation={'vertical'}/></td>
-			<td>{props.value}</td>
-		</tr>
-	)
+  const { t } = useTranslation()
+
+  if (!props.value) return null
+
+  return (
+    <tr className={"text-sm flex space-x-2"}>
+      <td className={"w-[2.5rem] shrink-0"}>
+        {t(`common:Team.Member.${props.name}`)}
+      </td>
+      <td>
+        <Separator orientation={"vertical"} />
+      </td>
+      <td>{props.value}</td>
+    </tr>
+  )
 }
 
+export const Member = ({ nameKey }: { nameKey: string }) => {
+  const { t } = useTranslation(LocaleNameSpace.manufacture)
+  const figure: string | null = t(`Team.${nameKey}.Figure`)
+  const hasFigure = figure && figure.startsWith("/")
+  const gender = t(`Team.${nameKey}.Gender`)
 
-export const Member = ({ nameKey }: {
-	nameKey: string
-}) => {
-	const { t } = useTranslation(LocaleNameSpace.manufacture)
-	const figure: string | null = t(`Team.${nameKey}.Figure`)
-	const hasFigure = figure && figure.startsWith('/')
-	const gender = t(`Team.${nameKey}.Gender`)
-	
-	
-	return (
-		<div className={'group grow flex flex-wrap md:flex-nowrap gap-2 bg-card text-gray-400'}>
-			<div key={nameKey} className={clsx(
-				'w-full md:w-[200px] shrink-0 flex flex-col items-center justify-end',
-			)}>
-				<Image src={hasFigure ? figure : `/images/members/mysterious-${gender}.png`}
-				       alt={nameKey}
-				       className={'drop-shadow group-hover:drop-shadow-lg'}
-				       width={160}
-				       height={0}
-				       style={{ width: 'auto' }} // 图像比例被修改了
-				/>
-				{hasFigure &&
-			<Image src={ImgMoZhen} alt={'MoZhen'} className={clsx(
-							// 'drop-shadow',
-							'group-hover:drop-shadow-lg',
-							'-mt-24 -mb-12 ',
-							'animate-pulse',
-							'brightness-75',
-						)} width={200}/>}
-			</div>
-			
-			<table className={'grow m-4'}>
-				<tbody className={'space-y-2'}>
-				<MemberProperty name={'Name'} value={t(`manufacture:Team.${nameKey}.Name`)}/>
-				<MemberProperty name={'Title'} value={t(`manufacture:Team.${nameKey}.Title`)}/>
-				<MemberProperty name={'Desc'} value={t(`manufacture:Team.${nameKey}.Desc`)}/>
-				<MemberProperty name={'Mail'} value={
-					<Link href={"mailto:"+t(`manufacture:Team.${nameKey}.Mail`)} className={'underline'}>
-						{t(`manufacture:Team.${nameKey}.Mail`)}
-					</Link>
-				}/>
-				</tbody>
-			</table>
-		</div>
-	)
+  return (
+    <div
+      className={
+        "group grow flex flex-wrap md:flex-nowrap gap-2 bg-card text-gray-400"
+      }
+    >
+      <div
+        key={nameKey}
+        className={clsx(
+          "w-full md:w-[200px] shrink-0 flex flex-col items-center justify-end",
+        )}
+      >
+        <Image
+          src={hasFigure ? figure : `/images/members/mysterious-${gender}.png`}
+          alt={nameKey}
+          className={"drop-shadow group-hover:drop-shadow-lg"}
+          width={160}
+          height={0}
+          style={{ width: "auto" }} // 图像比例被修改了
+        />
+        {hasFigure && (
+          <Image
+            src={ImgMoZhen}
+            alt={"MoZhen"}
+            className={clsx(
+              // 'drop-shadow',
+              "group-hover:drop-shadow-lg",
+              "-mt-24 -mb-12 ",
+              "animate-pulse",
+              "brightness-75",
+            )}
+            width={200}
+          />
+        )}
+      </div>
+
+      <table className={"grow m-4"}>
+        <tbody className={"space-y-2"}>
+          <MemberProperty
+            name={"Name"}
+            value={t(`manufacture:Team.${nameKey}.Name`)}
+          />
+          <MemberProperty
+            name={"Title"}
+            value={t(`manufacture:Team.${nameKey}.Title`)}
+          />
+          <MemberProperty
+            name={"Desc"}
+            value={t(`manufacture:Team.${nameKey}.Desc`)}
+          />
+          <MemberProperty
+            name={"Mail"}
+            value={
+              <Link
+                href={"mailto:" + t(`manufacture:Team.${nameKey}.Mail`)}
+                className={"underline"}
+              >
+                {t(`manufacture:Team.${nameKey}.Mail`)}
+              </Link>
+            }
+          />
+        </tbody>
+      </table>
+    </div>
+  )
 }
